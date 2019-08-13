3033a0d7c3063ef4a9d4210891bb042c91938b6b
Daniel Hill <dhill2522@gmail.com>
2019-08-13 11:14:09 -0600

    added initial warning modal

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,31 @@
 <template>
   <div id="app">
+    <div class="modal" :class="{ 'is-active': showWarning }">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">
+            To the User...
+          </p>
+          <buttton class="delete" @click="closeWarningModal()"></buttton>
+        </header>
+        <section class="modal-card-body">
+          <p>Drone-AWE is designed to be a versatile modeling package that reliably predicts drone performance. 
+            It has been developed based on a literature review of drone weather and battery performance published 
+            in Advanced Commercial Drone Economics by Darryl Jenkins et al. and other research available in the 
+            docs. Two stages remain to be conducted: first, wind tunnel data will be gathered and used to validate 
+            and refine the model; second, field testing will be performed for further validation and refinement. 
+            In its current state, Drone-AWE is NOT DESIGNED FOR PRECISE ENGINEERING APPLICATIONS. It is, however, 
+            useful for qualitative, back-of-the-envelope predictions and comparison.</p>
+          <p>If you have experimental data available for any drone and are interested in furthering open-source 
+              drone performance research, please make an issue <a href="https://github.com/rymanderson/drone_awe/issues">Github Respository</a>. Thank you for your support.
+          <p>- The Drone-AWE Team</p>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="is-success button" @click="closeWarningModal()">Close</button>
+        </footer>
+      </div>
+    </div>
     <div class="holder">
       <SideBar class="side-bar"/>
       <Main class="main"/>
@@ -18,7 +44,13 @@ import SideBar from './components/SideBar.vue'
     SideBar,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  private showWarning = true
+
+  private closeWarningModal(): void {
+    this.showWarning = false
+  }
+}
 </script>
 
 <style>
